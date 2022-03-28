0691d7f1494ed61eb9ac550d7e780202efdc330b
OwlPoisonDream <90618173+OwlPoisonDream@users.noreply.github.com>
2022-03-28 14:44:36 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-# сдесь находятся модели класса
+# здесь находятся модели класса
 from werkzeug.security import generate_password_hash,  check_password_hash # для создание хеша паролей
 from flask_login import  UserMixin 
 from app import db,login_manager
@@ -19,8 +19,8 @@ class User(db.Model,UserMixin):
     name = db.Column(db.String(100), nullable=False) #ФИО
     nickname = db.Column(db.String(20), nullable=True) # прозвище
     link_vk = db.Column(db.String(50), nullable=True) # ссылка на вк
-    inn = db.Column(db.String(12), nullable=True) # инн
-    bank_details = db.Column(db.String(100), nullable=True) # реквезиты банка  
+    inn = db.Column(db.String(12), nullable=True) # ИНН
+    bank_details = db.Column(db.String(100), nullable=True) # реквизиты банка  
     bankName = db.Column(db.String(100), nullable=True) # название банка
     phone_number = db.Column(db.String(11), nullable=True) # номер телефона
 
@@ -46,8 +46,8 @@ class Tasks(db.Model):
     nameTask = db.Column(db.String(255))# имя задачи
     descTask = db.Column(db.String(255))# Описание задачи
     timeTask = db.Column(db.String(255))# время выполнения
-    manyTask = db.Column(db.String(255))# денги за задачу
-    statusСompleted = db.Column(db.String(255))#статус выполненияы
+    manyTask = db.Column(db.String(255))# деньги за задачу
+    statusСompleted = db.Column(db.String(255))#статус выполнения
     receipt = db.Column(db.Float)# проверка чека
     linkDisk = db.Column(db.String(255))# ссылка на диск
 
