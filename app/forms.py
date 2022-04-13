644c812fd43feb9d64f3d8df82f2c2d61400f80c
SuperFish22 <37450477+SuperFish22@users.noreply.github.com>
2022-04-13 11:57:49 +0300

    Add files via upload

---
+++ app/forms.py
@@ -0,0 +1,83 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+import email_validator
+from app.models import User
+
+class LoginForm(FlaskForm): # Форма для логина с запоминанием пользователя
+    email = StringField("email", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    remember = BooleanField("remember Me")
+    submit = SubmitField()
+    
+
+class RegistrationForm(FlaskForm): # Форма регистрации
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('register')
+
+    def validate_username(self, username): #Проверка на несовпадение имён пользователя
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста, используйте другое имя пользователя.')
+
+    def validate_email(self, email): #проверка на несовпадение электронных почт
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста, используйте другой адрес электронной почты.')
+
+class ResetPasswordRequestForm(FlaskForm): # Форма восстановления пароля 
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm): # Форма сброса пароля
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
+
+class PersonalForm(FlaskForm): # Форма для заполнения таблицы users_Data
+    name = StringField('ФИО', validators=[DataRequired()]) #ФИО
+    nickname = StringField('Прозвище', validators=[DataRequired()]) # прозвище
+    link_vk = StringField('Ссылка на ВК', validators=[DataRequired()]) # ссылка на вк
+    birthDAy = StringField('Дата Рождения', validators=[DataRequired()])# Дата рождения 
+    passport = StringField('Номер паспорта', validators=[DataRequired()])# номер паспорта
+    passportData = StringField('Дата выдачи ', validators=[DataRequired()])# дата выдачи 
+    passportBy = StringField('Кем выдан паспорт', validators=[DataRequired()])# кем выдан паспорт
+    passportCod = StringField('Код подразделения', validators=[DataRequired()])# код подразделения
+    inn = StringField('ИНН', validators=[DataRequired()]) #инн
+    bank_details = StringField('Реквизиты банка ', validators=[DataRequired()]) # реквизиты банка  
+    bankName = StringField('Название банка', validators=[DataRequired()]) # название банка
+    phone_number = StringField('Номер телефона', validators=[DataRequired()]) # номер телефона
+    submit = SubmitField('Изменить форму')
+
+class CreateProject(FlaskForm):# создание проекта
+    projectName = StringField('Имя проекта', validators=[DataRequired()])# имя проекта
+    descProject = StringField('Описание', validators=[DataRequired()])# описание проекта 
+    linkDisk = StringField('ссылка на гугл диск', validators=[DataRequired()])# ссылка на гугл диск
+    submit = SubmitField('+ Создать проект')
+
+class CreateTask(FlaskForm):# создание задачи
+    idUser = StringField("ID пользователя", validators=[DataRequired()]) #id пользователя
+    idProject = StringField('Id проекта', validators=[DataRequired()])  # id проекта. Берёт из таблицы project
+    nameTask = StringField('Имя задачи', validators=[DataRequired()])# имя задачи
+    descTask = StringField('Описание задачи', validators=[DataRequired()])# Описание задачи
+    timeTask = StringField('время выполнения', validators=[DataRequired()])# время выполнения
+    manyTask = StringField('сколько заплатят', validators=[DataRequired()])# деньги за задачу
+    linkDisk = StringField('ссылка на гугл диск', validators=[DataRequired()])# ссылка на гугл диск
+    submit = SubmitField('Создать задачу')
+
+class ChangeTask(FlaskForm):# изменение задачи
+    id = StringField("Id задачи", validators=[DataRequired()]) #ID изменяемой задачи
+    idUser = StringField("ID пользователя", validators=[DataRequired()]) #id пользователя
+    idProject = StringField('Id проекта', validators=[DataRequired()])  # id проекта. Берёт из таблицы project
+    nameTask = StringField('Имя задачи', validators=[DataRequired()])# имя задачи
+    descTask = StringField('Описание задачи', validators=[DataRequired()])# Описание задачи
+    timeTask = StringField('время выполнения', validators=[DataRequired()])# время выполнения
+    manyTask = StringField('сколько заплатят', validators=[DataRequired()])# деньги за задачу
+    statusСompleted = StringField('Статус выполнения', validators=[DataRequired()]) # Статус выполнения
+    receipt = StringField('Чек', validators=[DataRequired()]) # Чек за работу
+    linkDisk = StringField('ссылка на гугл диск', validators=[DataRequired()])# ссылка на гугл диск
+    submit = SubmitField('Изменить задачу')
